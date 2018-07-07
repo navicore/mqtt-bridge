9e7d829d1de87b7257ad010a3cb2a70de6260519
Ed Sweeney <ed@onextent.com>
2018-07-07 08:31:54 -0700

    mergehub

--- src/main/scala/onextent/iot/mqtt/bridge/Stream.scala
+++ src/main/scala/onextent/iot/mqtt/bridge/Stream.scala
@@ -1,26 +1,33 @@
 package onextent.iot.mqtt.bridge
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import akka.NotUsed
-import akka.stream.ThrottleMode
+import akka.{Done, NotUsed}
+import akka.actor.ActorSystem
 import akka.stream.alpakka.mqtt._
 import akka.stream.alpakka.mqtt.scaladsl.{MqttSink, MqttSource}
-import akka.stream.scaladsl.{Flow, RestartSource, Source}
+import akka.stream.scaladsl.{Flow, MergeHub, RestartSource, RunnableGraph, Sink, Source}
+import akka.stream.{Materializer, ThrottleMode}
 import akka.util.ByteString
 import com.typesafe.scalalogging.LazyLogging
 import onextent.iot.mqtt.bridge.Conf._
 import onextent.iot.mqtt.bridge.models.SayHello
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.language.implicitConversions
-import scala.util.{Failure, Success}
 
-/**
-  * 2 streams - a cmd and a mqtt listener.
-  * it was a single sink with MergeHub but was failing silently, RestartSource wasn't seeing a future to watch...
-  * todo: why isn't it enough to have RestartSource, do I really need the onComplete??
-  */
 object Stream extends LazyLogging {
+
+  def createToConsumer(consumer: Sink[MqttMessage, Future[Done]])(
+      implicit s: ActorSystem,
+      m: Materializer): Sink[MqttMessage, NotUsed] = {
+    val runnableGraph: RunnableGraph[Sink[MqttMessage, NotUsed]] = {
+      MergeHub
+        .source[MqttMessage](perProducerBufferSize = 16)
+        .to(consumer)
+    }
+
+    runnableGraph.run()
+  }
 
   def throttlingFlow[T]: Flow[T, T, NotUsed] =
     Flow[T].throttle(
@@ -43,40 +50,25 @@ object Stream extends LazyLogging {
 
     logger.info(s"stream starting...")
 
-    val mqttStream = RestartSource
+    val toConsumer: Sink[MqttMessage, NotUsed] = createToConsumer(
+      MqttSink(sinkSettings, MqttQoS.atLeastOnce))
+
+    RestartSource
       .withBackoff(minBackoff = 1 second,
                    maxBackoff = 10 seconds,
                    randomFactor = 0.2) { () =>
         MqttSource.atMostOnce(srcSettings, 8)
       }
-      .runWith(MqttSink(sinkSettings, MqttQoS.atLeastOnce))
+      .runWith(toConsumer)
 
-    mqttStream onComplete {
-      case Success(_) =>
-        logger.warn("success. but hello stream should not end!")
-        actorSystem.terminate()
-      case Failure(e) =>
-        logger.error(s"failure. hello stream should not end! $e", e)
-        actorSystem.terminate()
-    }
-
-    val cmdStream = RestartSource
+    RestartSource
       .withBackoff(minBackoff = 1 second,
                    maxBackoff = 10 seconds,
                    randomFactor = 0.2) { () =>
         Source.fromGraph(new HelloSource()).via(throttlingFlow)
       }
       .map(helloMqttMessage())
-      .runWith(MqttSink(sinkSettings, MqttQoS.atLeastOnce))
-
-    cmdStream onComplete {
-      case Success(_) =>
-        logger.warn("success. but hello stream should not end!")
-        actorSystem.terminate()
-      case Failure(e) =>
-        logger.error(s"failure. hello stream should not end! $e", e)
-        actorSystem.terminate()
-    }
+      .runWith(toConsumer)
 
   }
 
