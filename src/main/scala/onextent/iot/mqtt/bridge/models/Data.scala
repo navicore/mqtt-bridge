58d8340fddd591544353d506bbe71a99b9257785
Ed Sweeney <ed@onextent.com>
2018-07-15 13:56:36 -0700

    heartbeat

--- src/main/scala/onextent/iot/mqtt/bridge/models/Data.scala
+++ src/main/scala/onextent/iot/mqtt/bridge/models/Data.scala
@@ -2,7 +2,7 @@ package onextent.iot.mqtt.bridge.models
 
 sealed trait Command
 
-case class Heartbeat(myName: String) extends Command {
+case class Heartbeat(myName: String) extends onextent.iot.mqtt.bridge.models.Command {
   def beat(): String = s"Heartbeat from $myName"
   def asJson(): String = s"""{"msg": "${beat()}"}"""
 }
